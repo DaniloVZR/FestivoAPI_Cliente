898697948a71a0d741cc90f8875c75accd397993
DaniloVZR <daniloud2809@gmail.com>
2025-05-17 20:40:06 -0500

    Material Angular

--- src/features/components/festivo/festivo.component.ts
+++ src/features/components/festivo/festivo.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ReferenciasMaterialModule } from '../../../shared/modules/referencias-material.module';
 
 @Component({
   selector: 'app-festivo',
-  imports: [],
+  imports: [ReferenciasMaterialModule],
   templateUrl: './festivo.component.html',
   styleUrl: './festivo.component.css'
 })
