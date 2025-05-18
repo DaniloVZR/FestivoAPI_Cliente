1110c8c18962ecfc81884742059d44f2f7b889e1
DaniloVZR <daniloud2809@gmail.com>
2025-05-18 01:16:37 -0500

    Estilos de la pagina

--- src/features/components/inicio/inicio.component.ts
+++ src/features/components/inicio/inicio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
-  imports: [RouterLink],
+  imports: [RouterLink, MatIcon],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
